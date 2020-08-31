66c8bd6454dd13c328265add4ab7d36977cc636b
JonathanGrant92 <45248168+JonathanGrant92@users.noreply.github.com>
2020-08-31 12:54:03 -0600

    Adding template code files for our project.
    Future additions/edits should be done on personal forked repos, and pull requested to this one.

---
+++ database.java
@@ -0,0 +1,20 @@
+class Database {
+    // #TODO: class variables, class methods
+
+    public static void create() {
+
+    }
+
+    public static void read() {
+
+    }
+
+    public static void update() {
+
+    }
+
+    public static void delete() {
+        
+    }
+
+}
---
+++ main.java
@@ -0,0 +1,10 @@
+//import org.apache.poi
+
+class main {
+    public static void main(final String[] args) {
+        System.out.println("Hello World");
+
+        //#TODO export/import data of excel file.
+            // Jonathan Grant will handle import/export.
+    }
+}
