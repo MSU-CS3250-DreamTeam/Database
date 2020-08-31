3fc703ce0a680f8707365914edfb5ba636a109ba
JonathanGrant92 <45248168+JonathanGrant92@users.noreply.github.com>
2020-08-31 16:20:10 -0600

    Added constructor and fixed indentation.

--- database.java
+++ database.java
@@ -1,20 +1,28 @@
 class Database {
-    // #TODO: class variables, class methods
+  // #TODO: class variables, class methods
+    
+  // A list of class private variables.
+  
+  
+  // Constructor
+  public Database() {
+    
+  }
 
-    public static void create() {
+  public static void create() {
+    
+  }
 
-    }
+  public static void read() {
+    
+  }
 
-    public static void read() {
+  public static void update() {
+    
+  }
 
-    }
-
-    public static void update() {
-
-    }
-
-    public static void delete() {
-        
-    }
+  public static void delete() {
+    
+  }
 
 }
