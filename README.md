8d0424df1955782020c042cce180551eca89018e
JonathanGrant92 <45248168+JonathanGrant92@users.noreply.github.com>
2020-09-01 09:23:55 -0600

    Implemented the import algorithm to extract data from spreadsheet and send to an object of Database. Debugged to working state. (#11)

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 
 Future additions/edits should be done on personal forked repos, and pull requested to this one.
 
+# Notice:
+## Copy inventory_team1.csv to root directory (one up) for program to find it.
+
 Git/Github Guide:
 https://arccwiki.uwyo.edu/index.php/Git_and_GitHub
--- main.java
+++ main.java
@@ -1,11 +1,59 @@
-//import org.apache.poi
+package Database; // Parent directory should be named "Database"
 
-class main {
-  public static void main(final String[] args) {
-    // Test output for debugging purposes.
-    System.out.println("Hello World");
+//import org.apache.poi
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import Database.*;
+
+import java.util.Arrays; // For debugging purposes. Removing will break column labels output test.
+
+/**
+ * 
+ */
+public class main {
+
+  static private final String SPREAD_SHEET = "inventory_team1.csv";
+  static private Database new_database;
+
+  /**
+   * 
+   * @param args
+   * @throws FileNotFoundException
+   */
+  static public void main(String[] args) throws FileNotFoundException {
+
+    System.out.println("Hello World");  // Testing output for debugging purposes. Should remove on final project.
     
-    //TODO export/import data of excel file.
+    //TODO export/write data to excel file, if we need to for update().
+    
+    File new_file = new File(SPREAD_SHEET);
+    if (!new_file.exists()) {
+      throw new FileNotFoundException("Is the data file " + SPREAD_SHEET + " in the wrong directory?"); // For debugging.
+    }
+
+    Scanner data_input = new Scanner(new_file);
+    if (data_input.hasNextLine()) {
+      String[] data_row = data_input.nextLine().split(",");
+      new_database = new Database(data_row.length);
+      new_database.create(data_row);
+
+      // For debugging purposes.
+      System.out.println("You should see the column labels if program read the populated spreadsheet: ");
+      System.out.println(Arrays.toString(data_row));
+
+    }
+    
+    
+
+    while (data_input.hasNextLine()) {
+
+      String[] data_row = data_input.nextLine().split(",");
+      new_database.create(data_row);
+
+    }
+
+    data_input.close();
 
   }
 }
