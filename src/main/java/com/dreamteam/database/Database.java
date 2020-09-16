8efb4ccd1c4e5248d74aee80bad932cff8409577
alexandersanford <alex.b.sanford@gmail.com>
2020-09-16 10:53:51 -0600

    Added buyer_event.csv, commented out draft method

--- src/main/java/com/dreamteam/database/Database.java
+++ src/main/java/com/dreamteam/database/Database.java
@@ -134,5 +134,14 @@ public class Database {
     }
 
   }
-
+  /*public void buyerEvent(String[] new_data, String id) {
+  	System.out.println("We have a new buyer event!");
+  	Boolean entryExists = false;
+  	if (read(id) == null) {
+  		create(new_data);
+	}
+  	
+  	
+  	
+  }*/
 }
