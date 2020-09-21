0096a197cac7075652c26fabe1bb94fd93af90cd
Alejandro-Rojas <rojas.alejandro303@gmail.com>
2020-09-20 21:44:49 -0600

    junit testing

---
+++ src/test/java/com/dreamteam/database/OrderHistoryTest.java
@@ -0,0 +1,26 @@
+package com.dreamteam.database;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class OrderHistoryTest {
+    @Test
+    public void customerOrder() {
+        System.out.println("This is the testcase in this class");
+        String str1="This is the testcase in this class";
+        assertEquals("This is the testcase in this class", str1);
+    }
+
+
+
+    @Test
+    public void Date() {
+
+
+    }
+@Test
+    public void Time() {
+
+    }
+}
