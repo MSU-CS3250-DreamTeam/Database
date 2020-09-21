ac1b8e58c8857e09c3fd6e9d5486c63c6e57c5fc
kevvo15 <58575080+kevvo15@users.noreply.github.com>
2020-09-21 00:02:55 -0600

    jUnit Test for BuyerEvent.java. Do not merge yet
    This test will coincide with the buyer simulation. See pull request #52. Still needs to be developed alongside the updateQuantity method in the BuyerEvent.java file.

---
+++ src/test/java/com/dreamteam/database/BuyerEventTest.java
@@ -0,0 +1,12 @@
+package com.dreamteam.database;
+
+import org.junit.Test;
+
+public class BuyerEventTest {
+
+    @Test
+    public void testUpdateQuantity(){
+        //method still in progress
+    }
+
+}
